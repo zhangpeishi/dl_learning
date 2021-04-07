ff82d5ecee30c79a29c50f454d08f71bd5f2e3f5
zhangpeishi <zhangpeishi276@gmail.com>
2021-04-07 19:04:51 +0800

    4.7

--- d2lzh_pytorch.py
+++ d2lzh_pytorch.py
@@ -102,13 +102,28 @@ def load_data_fashion_mnist(batch_size, root='~/Datasets/FashionMNIST'):
     
     
 # 我们可以评价模型net在数据集data_iter上的准确率
-def evaluate_accuracy(data_iter, net):
-    acc_sum, n = 0.0, 0
-    for X, y in data_iter:
-        acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+# def evaluate_accuracy(data_iter, net):
+#     acc_sum, n = 0.0, 0
+#     for X, y in data_iter:
+#         acc_sum += (net(X).argmax(dim=1) == y).float().sum().item()
+#         n += y.shape[0]
+#     return acc_sum / n
+#3.13修改
+def evaluate_accuracy(data_iter,net):
+    acc_sum,n = 0.0,0
+    for X,y in data_iter:
+        if isinstance(net,torch.nn.Module):
+            net.eval() #评估模式，这会关闭dropout
+            acc_sum += (net(X).argmax(dim = 1) == y).float().sum().item()
+            net.train() #改回训练模式
+        else:#自定义的模型
+            if('is_training' in net.__code__.co_varnames):#如果有is_training 这个参数
+                #将is_training设置成false
+                acc_sum += (net(X,is_training= False).argmax(dim = 1)==y).float().sum().item()
+            else:
+                acc_sum += (net(X).argmax(dim = 1) == y).float().sum().item()
         n += y.shape[0]
-    return acc_sum / n
-    
+    return acc_sum / n    
     
     
 
