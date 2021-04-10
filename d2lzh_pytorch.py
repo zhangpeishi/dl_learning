f90a6684bfb4fe3be03a07e74a14da0ef69e76b3
zhangpeishi <zhangpeishi276@gmail.com>
2021-04-10 20:35:33 +0800

    4.10

--- d2lzh_pytorch.py
+++ d2lzh_pytorch.py
@@ -180,5 +180,11 @@ def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
         plt.legend(legend)
 
     
-    
+def corr2d(X, K):  # 本函数已保存在d2lzh_pytorch包中方便以后使用
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i: i + h, j: j + w] * K).sum()
+    return Y    
     
